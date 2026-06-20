6ffbc693c4fa86a978d4363870ab528b31a38ef1
mariane-mj <marianemourajurema@gmail.com>
2026-06-20 08:17:45 -0300

    Fix quotes array and newQuote function formatting

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var quotes = [
     'O maior goleador geral é o alemão Miroslav Klose, com 16 gols. O brasileiro Ronaldo Fenômeno vem logo atrás, com 15.',
     'Lionel Messi é o jogador com mais partidas disputadas na história do torneio, acumulando 26 jogos.',
     'A maior goleada da história das Copas ocorreu em 1982, quando a Hungria venceu El Salvador por 10 a 1.',
-    'O nome "Copa" vem do francês coupe, que significa taça, o prêmio original entregue aos campeões.'
+    'O nome "Copa" vem do francês coupe, que significa taça, o prêmio original entregue aos campeões.';
     ' A Copa de 1950, sediada no Brasil, foi a única a não ter uma final oficial. O campeão foi decidido através de um quadrangular final.'
 ]
 
